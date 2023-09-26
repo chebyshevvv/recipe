70ab224cb88769b13f32c308a7b6a9771d5161e8
赵维康 <chebyshev_zwk@outlook.com>
2023-09-26 17:15:32 +0800

    3

--- core/src/main/java/com/zh/core/controller/RecipeController.java
+++ core/src/main/java/com/zh/core/controller/RecipeController.java
@@ -1,12 +1,8 @@
 package com.zh.core.controller;
 
-import cn.hutool.core.util.IdUtil;
 import com.zh.common.http.HttpResult;
 import com.zh.core.model.Recipe;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,7 +15,7 @@ public class RecipeController {
     public HttpResult recipes(){
         return HttpResult.ok(recipes);
     }
-    @GetMapping("add")
+    @PostMapping("add")
     public HttpResult add(@RequestBody Recipe recipe){
         recipes.add(recipe);
         return HttpResult.ok();
