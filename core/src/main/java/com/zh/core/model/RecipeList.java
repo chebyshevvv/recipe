ad1293a2171d8aa65725b21418cfe6601da5c403
赵维康 <chebyshev_zwk@outlook.com>
2023-09-28 16:49:58 +0800

    5

---
+++ core/src/main/java/com/zh/core/model/RecipeList.java
@@ -0,0 +1,11 @@
+package com.zh.core.model;
+
+import lombok.Data;
+
+@Data
+public class RecipeList {
+    private String id;
+    private String name;
+    private String description;
+    private Integer sort;
+}
